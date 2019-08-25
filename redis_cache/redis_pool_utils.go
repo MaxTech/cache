6810effbfec848b7ff01961864d1ac517b3157ad
Maximilian <maximilian520@163.com>
2019-08-25 16:56:58 +0800

    update

--- redis_cache/redis_pool_utils.go
+++ redis_cache/redis_pool_utils.go
@@ -11,8 +11,14 @@ type redisPoolUtils struct {
 
 var RedisPoolUtils *redisPoolUtils
 
-func (rpu *redisPoolUtils) InitRedisPool(address string, password string, dbNum int) *redis.Pool {
-    return &redis.Pool{
+type RedisPool interface {
+    CheckRedisPool() bool
+}
+
+type pool redis.Pool
+
+func (rpu *redisPoolUtils) InitRedisPool(address string, password string, dbNum int) RedisPool {
+    temp := pool(redis.Pool{
         MaxIdle:     500,
         MaxActive:   50,
         IdleTimeout: 60 * time.Second,
@@ -29,11 +35,12 @@ func (rpu *redisPoolUtils) InitRedisPool(address string, password string, dbNum 
             }
             return con, nil
         },
-    }
+    })
+    return &temp
 }
 
-func (rpu *redisPoolUtils) InitRedisPoolByConfig(redisConfig RedisConfigFormat) *redis.Pool {
-    return &redis.Pool{
+func (rpu *redisPoolUtils) InitRedisPoolByConfig(redisConfig RedisConfigFormat) RedisPool {
+    temp := pool(redis.Pool{
         MaxIdle:     500,
         MaxActive:   50,
         IdleTimeout: 60 * time.Second,
@@ -50,17 +57,21 @@ func (rpu *redisPoolUtils) InitRedisPoolByConfig(redisConfig RedisConfigFormat) 
             }
             return con, nil
         },
-    }
+    })
+    return &temp
 }
 
-func (rpu *redisPoolUtils) CheckRedisPool(redisPool *redis.Pool) bool {
-    conn := redisPool.Get()
+func (r *pool) CheckRedisPool() bool {
+    conn, err := r.Dial()
+    if err != nil {
+        return false
+    }
     defer conn.Close()
     if conn.Err() != nil {
         log.Println("redis connect error:", conn.Err().Error())
         return false
     }
-    _, err := redis.String(conn.Do("ping"))
+    _, err = redis.String(conn.Do("ping"))
     if err != nil {
         log.Println("redis connect ping error:", err.Error())
         return false
